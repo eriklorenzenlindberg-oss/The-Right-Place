1eaa329a31dc1880f408b7644e6833127dc8576f
eriklorenzenlindberg-oss <eriklorenzenlindberg@gmail.com>
2026-09-20 22:08:00 +0200

    Update semicircles.py

--- semicircles.py
+++ semicircles.py
@@ -168,7 +168,7 @@ def render(math_data):
 
         fig.add_trace(go.Scatter(
             x=x_lines, y=y_lines, mode="lines", 
-            line=dict(color=line_color, width=1), 
+            line=dict(color=line_color, width=0.5), 
             hoverinfo="skip", showlegend=False
         ))
         
