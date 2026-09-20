a87af7f017dd788d09740e90fafc6dd41c8560a4
eriklorenzenlindberg-oss <eriklorenzenlindberg@gmail.com>
2026-09-20 21:32:37 +0200

    Update iso.py

--- iso.py
+++ iso.py
@@ -120,8 +120,8 @@ def render(
     pos2 = generate_positions(nodes, lengths2, directions2)
 
     # Tema-inställningar från Streamlit
-    line_color = st.get_option("theme.primaryColor") 
-    bg_color = st.get_option("theme.backgroundColor") 
+    line_color = #000000
+    bg_color = #FFFFFF 
 
     # Skapa figuren med subplots
     fig = make_subplots(
