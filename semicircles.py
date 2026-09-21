429714eb190d0be58d86a02d034b7e1cf1527634
eriklorenzenlindberg-oss <eriklorenzenlindberg@gmail.com>
2026-09-21 18:28:45 +0200

    Update semicircles.py

--- semicircles.py
+++ semicircles.py
@@ -96,7 +96,7 @@ def render(math_data):
         return
 
     # Breddförhållande för kolumnerna
-    col_plot, col_controls = st.columns()
+    col_plot, col_controls = st.columns([6, 3])
     
     with col_controls:
         max_overlap = False
