5ac01827fdacbec0a077ba15ada2321fad546921
eriklorenzenlindberg-oss <eriklorenzenlindberg@gmail.com>
2026-09-21 19:49:28 +0200

    Update semicircles.py

--- semicircles.py
+++ semicircles.py
@@ -1,6 +1,6 @@
 import numpy as np
 import plotly.graph_objects as go
-import streamlit st
+import streamlit as st
 
 # --------------------------------------------------
 # TEXTFORMATERING FÖR MATEMATISKA TEXTER
