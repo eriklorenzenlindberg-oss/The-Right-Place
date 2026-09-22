f97a1429ed189adadc2fac11c9d7f75c9491673e
eriklorenzenlindberg-oss <eriklorenzenlindberg@gmail.com>
2026-09-22 10:47:29 +0200

    Update app.py

--- app.py
+++ app.py
@@ -9,15 +9,10 @@ import sympy as sp
 
 st.subheader("The Right Place")
 
-# --- INGRESS (Syns alltid) ---
-st.markdown(
-    """
-    Visualizing generalizations of the golden ratios and self-similarity in n-dimensional space
-    """
-)
+
 
 # --- BRÖDTEXT (Utfällbar "Läs mer") ---
-with st.expander("Läs mer"):
+with st.expander("Visualizing generalizations of the golden ratio and self-similarity in n-dimensional space"):
     st.markdown(
         """
         Lorem ipsum Lorem ipsum Lorem ipsum
