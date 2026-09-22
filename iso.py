36b8d87e2dc94fa27bdbc4d21a088269071bf43c
eriklorenzenlindberg-oss <eriklorenzenlindberg@gmail.com>
2026-09-22 18:48:49 +0200

    Update iso.py

--- iso.py
+++ iso.py
@@ -281,7 +281,7 @@ def render(math_data):
             yaxis2=dict(visible=False, range=[ymin, ymax])
         )
 
-        fig.update_yaxes(anchor="top")
+        fig.update_xaxes(matches='x')
 
         st.plotly_chart(
             fig,
