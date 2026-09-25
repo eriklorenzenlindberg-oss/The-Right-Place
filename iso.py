60a7ad3a54ed1eb91e3e96c6a3aeea6c17f3153a
eriklorenzenlindberg-oss <eriklorenzenlindberg@gmail.com>
2026-09-25 16:15:13 +0200

    Update iso.py

--- iso.py
+++ iso.py
@@ -269,7 +269,7 @@ def render(math_data):
 
         # --- LAYOUT OCH ABSOLUT AXELLÅSNING ---
         fig.update_layout(
-            height=600,
+            height=300,
             plot_bgcolor=bg_color,
             paper_bgcolor=bg_color,
             showlegend=False,
